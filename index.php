674df127a31d5532ae351cc34fc1908404f728b3
Jason Tan <jasontxf@hotmai.com>
2012-09-19 19:36:26 +0800

    Initial

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$con=mysql_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
+
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+   mysql_select_db ("gallery");  
+
+
+$stmt = "INSERT INTO account (email, password) VALUES('jasontxf@hotmail.com', 'hahahaha')";
+$query = mysql_query($stmt);
+
+$stmt = "INSERT INTO account (email, password) VALUES('haha@hotmail.com', 'hahahaha')";
+$query = mysql_query($stmt);
+
+
+$stmt = "DELETE FROM account WHERE email = 'jasontxf@hotmail.com'";
+$query = mysql_query($stmt);
+
+$stmt = "UPDATE account SET password = 'leeweijian' WHERE email = 'haha@hotmail.com'";
+$query = mysql_query($stmt);
+
+
+  
+?>
